b163af0a2be340d629364b32f51c16eda3814920
Vaishnavi Nagrale <Vaishnavinagrale2@gmail.com>
2023-01-20 13:35:45 +0530

    files added

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 100.0,
+                  backgroundImage: AssetImage('assets/dice1.png'),
+                  backgroundColor: Colors.grey,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('Vaishnavi Nagrale',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      //fontStyle: FontStyle.italic
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text('Flutter Developer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 200.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  //padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                            Icons.phone,
+                            size: 25.0,
+                            color: Colors.green,
+                        ),
+                        SizedBox(
+                          width: 45.0,
+                        ),
+                        Text('09096101838',
+                           style: TextStyle(
+                             color: Colors.black,
+                             fontSize: 15.0,
+                           ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Card(
+                  //padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(0.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        size: 30.0,
+                        color: Colors.blue,
+                      ),
+                      trailing: Text('k_b_nagrale@gmail.com',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/practice.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+//class defination
+// class Human{
+//      void printNmae(String name){
+//        print(name);
+//      }
+// }
+void main(){
+  var map_name={
+    'key1':'valu1',
+    'key2':'2',
+    'key3':'3.3',
+
+  };
+  print(map_name);
+  // var n=stdin.readLineSync();
+  // var name=Human(); // object creation
+  // name.printNmae(n!);
+  // var list1=[1,2,3,4,5];  // collection just like array
+  // list1.add(6);
+  // var list2=[];
+  // list2.addAll(list1);
+  // list1.insert(2,7);
+  // list2.insertAll(3, list1);
+  // print("$list1");
+  // print("$list2");
+  // list2.removeLast();
+  // list2.remove(3);
+  // list2.removeAt(3);
+  // list2.removeRange(0, 5);
+  // print("$list2");
+  // print('my name is vaish');//printing
+  // stdout.write('enter your name');//printing
+  // var name=stdin.readLineSync();//input taking
+  // print("welcome, $name");//output with input variable
+  // var jahnavi = Human();//creating class object
+  //  // Decleratin of variable
+  //  int? a;
+  //  print(a);
+  //  a=5;
+  //  num b=23.233;
+  //  num c=3;
+  //  BigInt longint=BigInt.parse('2398904092890283209');
+  //  bool isLogin=false;
+  //  //intline decleration
+  // String n="vaish";
+  //
+  // String x="Khushi";
+  // var subject="maths";
+  //
+  // var Rollno=33;
+  //
+  // var section;
+  // section="0";//string
+  // section=false;//boolean
+  //
+  // dynamic dummy;
+}
